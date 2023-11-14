dca80f0e3fb3625c7ad7d175ee9676e6170663e8
Juan Canto <pablo.canto@gmail.com>
2023-11-14 09:15:02 -0600

    09. Buscando TODOs

--- src/App.js
+++ src/App.js
@@ -10,13 +10,22 @@ import React from 'react';
 const defaultTodos = [
   {text: 'Cortar cebolla', completed: true},
   {text: 'Tomar el curso de intro a React.js', completed: false},
-  {text: 'Lorrar con la Llorona', completed: false},
+  {text: 'Llorar con la Llorona', completed: false},
   {text: 'Lalala', completed: false},
 ]
 
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
   const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
+
   console.log('Los usuarios buscan ToDos de: ' + searchValue);
 
   return (
@@ -28,7 +37,7 @@ function App() {
       />
 
       <TodoList>
-       {defaultTodos.map(todo => (
+       {searchedTodos.map(todo => (
         <TodoItem 
            key={todo.text} 
            text = {todo.text}
