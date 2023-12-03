4646c9679929dd940cd77bfd129c40e790f2b2e8
Juan Canto <pablo.canto@gmail.com>
2023-12-03 01:59:55 -0600

    22. ¿Qué es React Context?

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,45 +6,43 @@ import {TodosLoading} from '../TodosLoading';
 import {TodosError} from '../TodosError';
 import {EmptyTodos} from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoContext } from '../TodoContext';
 
 
-function AppUI({
-    loading,
-    error,
-    completedTodos,
-    totalTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-})
-{
+
+function AppUI(){
     return (
         <>
-          <TodoCounter completed={completedTodos} total={totalTodos}/>
-          <TodoSearch
-           searchValue = {searchValue}
-           setSearchValue = {setSearchValue}
-          />
-    
-          <TodoList>
-            {loading && < TodosLoading />}
-            {error && < TodosError />}
-            {(!loading && searchedTodos.lenght === 0) &&  <EmptyTodos/>}
+          <TodoCounter/>
+          <TodoSearch/>
 
-           
-           {searchedTodos.map(todo => (
-            <TodoItem 
-               key={todo.text} 
-               text = {todo.text}
-               completed = {todo.completed}
-               onComplete={() => completeTodo(todo.text)}
-               onDelete={() => deleteTodo(todo.text)}
-            />
-           ))}
-           {}
-          </TodoList>
+          <TodoContext.Consumer>
+            {({
+                loading,
+                error,                
+                searchedTodos,
+                completeTodo,
+                deleteTodo,
+            }) => (
+              <TodoList>
+              {loading && < TodosLoading />}
+              {error && < TodosError />}
+              {(!loading && searchedTodos.lenght === 0) &&  <EmptyTodos/>}
+
+            
+            {searchedTodos.map(todo => (
+              <TodoItem 
+                key={todo.text} 
+                text = {todo.text}
+                completed = {todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+            {}
+              </TodoList>
+            )}
+          </TodoContext.Consumer>
     
           <CreateTodoButton/> 
     
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -2,7 +2,7 @@ import './TodoCounter.css';
 
 function TodoCounter({total, completed}){
     return(
-      total == completed?
+      total === completed?
       <h1 className='TodoCounter'>Felicidades, terminaste todas tus tareas!! </h1>
       :
       <h1 className='TodoCounter'>
