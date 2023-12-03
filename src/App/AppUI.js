697bd65f5b25c6ae12ed6b2e6cb23af7c9b00286
Juan Canto <pablo.canto@gmail.com>
2023-12-03 06:22:41 -0600

    24. ¿Qué son los React Portals?

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,6 +7,7 @@ import {TodosLoading} from '../TodosLoading';
 import {TodosError} from '../TodosError';
 import {EmptyTodos} from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
+import {Modal} from '../Modal';
 import { TodoContext } from '../TodoContext';
 
 
@@ -18,6 +19,8 @@ function AppUI(){
       searchedTodos,
       completeTodo,
       deleteTodo,
+      openModal,
+      setOpenModal,
     } = React.useContext(TodoContext);
 
     return (
@@ -42,6 +45,12 @@ function AppUI(){
             {}
               </TodoList>    
           <CreateTodoButton/> 
+         
+        {openModal && (
+          <Modal>
+            La funcionalidad de agregar TODO
+          </Modal>
+        )}
     
         </>
       );
